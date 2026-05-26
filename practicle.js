e6b87fec77fc44a3f69cb84c8baf410461a1a9c7
Sujal S. negi abes <sujalnegi657@gmail.com>
2026-05-26 16:00:17 +0530

    Add files via upload

---
+++ practicle.js
@@ -0,0 +1,15 @@
+function generateTable() {
+  let num = document.getElementById("number").value;
+  let result = document.getElementById("result");
+
+  
+
+  num = Number(num);
+  let output = "";
+
+  for (let i = 1; i <= 10; i++) {
+    output += num + " x " + i + " = " + (num * i) + "<br>";
+  }
+
+  result.innerHTML = output;
+}
